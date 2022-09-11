03be0fa5c78f9dc305a06b906c59870bcf7b6a6d
Rachelbaa <rach.fbb@gmailcom>
2022-09-11 15:46:04 +0300

    vue-config change

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
 const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+module.exports =  defineConfig({
+  publicPath: process.env.NODE_ENV === "production" ? "/Rachel-Furman-08-09-2022/" : "/",
+});
