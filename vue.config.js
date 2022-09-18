3d7f59752caac2c29f66d16bb4d7a9ab90fa2189
Rachelbaa <rach.fbb@gmailcom>
2022-09-19 00:15:16 +0300

    config

--- vue.config.js
+++ vue.config.js
@@ -1,25 +1,25 @@
 const { defineConfig } = require('@vue/cli-service')
-// module.exports = {
-//   publicPath: process.env.NODE_ENV === 'production'
-//     ? '/Rachel-Furman-08-09-2022/'
-//     : '/'
-// }
-
-export default defineConfig({
+module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
     ? '/Rachel-Furman-08-09-2022/'
     : '/'
-  ,
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: `
-          @import "./src/styles/setup/_mixins.scss";
-          @import "./src/styles/setup/_typography.scss";
-          @import "./src/styles/setup/_variables.scss";
-        `,
-      },
-    },
-  },
-});
+}
+
+// export default defineConfig({
+//   publicPath: process.env.NODE_ENV === 'production'
+//     ? '/Rachel-Furman-08-09-2022/'
+//     : '/'
+//   ,
+//   css: {
+//     preprocessorOptions: {
+//       scss: {
+//         additionalData: `
+//           @import "./src/styles/setup/_mixins.scss";
+//           @import "./src/styles/setup/_typography.scss";
+//           @import "./src/styles/setup/_variables.scss";
+//         `,
+//       },
+//     },
+//   },
+// });
 
